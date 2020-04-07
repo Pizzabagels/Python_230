b86522146ac3b1b6421aca30b3b7e72cae9e508e
Tommy <taguilu01@gmail.com>
2020-04-06 23:00:46 -0700

    Adding new files for mailroom

--- main.py
+++ main.py
@@ -11,11 +11,22 @@ app = Flask(__name__)
 def home():
     return redirect(url_for('all'))
 
+
 @app.route('/donations/')
 def all():
     donations = Donation.select()
     return render_template('donations.jinja2', donations=donations)
-    
+
+
+@app.route('/new', methods=['GET', 'POST'])
+def new_donation():
+    if request.method == "POST":
+        return redirect((url_for('donations')))
+    elif request.method == "GET":
+        new_donation = Donation(name=request.form['name'], donation=request.form['donation'])
+        new_donation.save()
+    return render_template('new_donor.jinja2', donations=Donation.select())
+
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 6738))
